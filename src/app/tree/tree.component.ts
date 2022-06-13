255bfdd6f1c109d7efcc0fa6312711db741a3eff
adarsh <02130adarsh@gmail.com>
2022-06-13 21:53:58 +0530

    angular-tree

--- src/app/tree/tree.component.ts
+++ src/app/tree/tree.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { TreeTableData, TreeTableHeaderObject, TreeTableRow } from 'angular-tree-table';
+import { Component, Input, OnInit } from '@angular/core';
+import { RouterEvent } from '@angular/router';
+import { TreeTableData, TreeTableHeaderObject, TreeTableRow, TreeTableRowAction, TtDataType } from 'angular-tree-table';
+
 
 @Component({
   selector: 'app-tree',
@@ -13,7 +15,9 @@ export class TreeComponent implements OnInit {
 
   tableConfig = {
     showExpandArrows: true, // Showing Arrows each possible row
-    showExpandAllArrows: true // Expand all button
+    showExpandAllArrows: true, // Expand all button
+    rowClickablesContext: null,
+    rowClickables: {}
   };
 
   constructor() { }
@@ -27,23 +31,32 @@ export class TreeComponent implements OnInit {
   populateDummyData() {
     const data = [];
     for (let i = 0; i < 120; i++) {
-      const row = new TreeTableRow(i + '', { sno: i+1, name: 'John '+(i+1), age: i+1}, false, new TreeTableData);
-      if (i % 10 !== 0) {
-        row.expandable = true;
-        const subTableData = new TreeTableData(this.tableConfig); //We can add new config object if required
+      
+
+      const subTableData = new TreeTableData(this.tableConfig); //We can add new config object if required
+      // if (i % 10 !== 0) {
+        // row.expandable = true;
         const subData = [];
-        for (let j = 0; j < (10 - i % 10); j++) {
-          const subRow = new TreeTableRow(j + '', { sno: j + 1, name: 'Paul ' + (j + 1), age: j + 1}, false, new TreeTableData);
+        for (let j = 0; j < (10 - i); j++) {
+          const subRow = new TreeTableRow(j + '', { sno: j + 1, name: 'Paul ' + (j + 1), age: j + 1,}, false, new TreeTableData);
           subData.push(subRow);
         }
         subTableData.headers = this.tableHeaders; //Using the same headers as parent table, we can use separate if required
         subTableData.data = subData;
-        row.children = subTableData;
-      }
+        // row.children = subTableData;
+      // }
+
+      const row = new TreeTableRow(i + '', { sno: i+1, name: 'John '+(i+1), age: i+1}, true, subTableData);
+      const action = new TreeTableRowAction("info",'info',"btn btn-secondary", this.info);
+      const removeAction = new TreeTableRowAction("delete",'delete',"btn btn-secondary",this.delete);
+      removeAction.context = this;
+      action.context=this;
+      row.actions.push(action,removeAction);
+      row.clickablesContext= this;
+      row.clickables= {showData: this.show}
       data.push(row);
     }
-    this.tableData = new TreeTableData(this.tableConfig);
-    this.populateHeaders();
+    
     this.tableData.data = data;
   }
   
@@ -53,7 +66,25 @@ export class TreeComponent implements OnInit {
     this.tableHeaders.push(new TreeTableHeaderObject('Name', 'name', "", true));
     this.tableHeaders.push(new TreeTableHeaderObject('Age', 'age', "", true));
     this.tableHeaders.push(new TreeTableHeaderObject('User Details', '=CONCAT(Name: |||name|||<br/>|||Age: |||age)', "", true));
-    // this.tableHeaders.push(new TreeTableHeaderObject('D.no', 'address.dno', "", true));
+    this.tableHeaders.push(new TreeTableHeaderObject('show', 'showData', "", true));
+    const actionHeader = new TreeTableHeaderObject('Action',"", "", true);
+    actionHeader.dataType = TtDataType.ACTIONS;
+    this.tableHeaders.push(actionHeader);
+    
+    //this.tableHeaders.push(new TreeTableHeaderObject('D.no', 'address.dno', "", true));
     this.tableData.headers = this.tableHeaders;
   }
+  info(data: any){
+    alert('action works')
+    console.log(data)
+  }
+  delete(){
+    alert('do ou really ena')
+  }
+  show(data : any){
+    alert("Name "+data.name +" ,Age "+data.age)
+  }
+  
 }
+
+
